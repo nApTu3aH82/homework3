68571a5b4deabcef61bfa514110abaa794fb0fe5
nApTu3aH82 <rinsy89@gmail.com>
2024-06-03 10:57:21 +0500

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+name = 'Ринат'
+print("Ваше имя", name)
+age = 41
+print('Ваш возраст:', age)
+age = age + 1
+print('Через год вам будет', age)
+is_student = True
+if is_student == True:
+    print('Вы студент', is_student)
